7594ce07ee02d212a8cba3686c911a1e7fcfbbd8
t7h8qhmb2r-alt <t7h8qhmb2r@privaterelay.appleid.com>
2026-04-17 11:40:14 +0900

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import "./globals.css";
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ja">
+      <head><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/></head>
+      <body>{children}</body>
+    </html>
+  );
+}
