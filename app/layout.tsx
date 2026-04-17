ac0c290771a88d2893a45c3a340ce6839311be44
t7h8qhmb2r-alt <t7h8qhmb2r@privaterelay.appleid.com>
2026-04-17 11:48:12 +0900

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-import "./globals.css";
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="ja">
-      <head><meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/></head>
-      <body>{children}</body>
-    </html>
-  );
-}
